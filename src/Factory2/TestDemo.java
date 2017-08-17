5f9ae0717e35ffdb46e0a55728a41c584146551d
小小火影_ <lt19921124@gmail.com>
2017-08-17 10:52:04 +0800

    k
    k

---
+++ src/Factory2/TestDemo.java
@@ -0,0 +1,50 @@
+package Factory2;
+
+interface IFruit {
+	
+	public void eat();//³ÔË®¹û
+	
+}
+
+class Factory {
+	public static IFruit getInstance(String className) {
+		
+		if ("apple".equals(className)) {
+			return new Apple();
+		}
+		if ("orange".equals(className)) {
+			return new Orange();
+		}
+		return null;
+	}
+}
+
+class Apple implements IFruit {
+	public void eat() {
+		System.out.println("Ï÷Æ¤³ÔÆ»¹û");
+	}
+}
+
+class Orange implements IFruit {
+	public void eat() {
+		System.out.println("°þÆ¤³ÔéÙ×Ó");
+	}
+}
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IFruit fruit = new Factory().getInstance(args[0]);
+		fruit.eat();
+		
+		
+	}
+
+}
+
+
+
+
+
+
