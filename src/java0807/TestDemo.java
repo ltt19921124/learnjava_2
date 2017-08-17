f316eb3844a2efde119e364154c15524f4361d2b
小小火影_ <lt19921124@gmail.com>
2017-08-17 10:45:33 +0800

    k
    k

--- src/java0807/TestDemo.java
+++ src/java0807/TestDemo.java
@@ -1,9 +1,15 @@
 package java0807;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class TestDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Vector v = new Vector();
+		ArrayList al = new ArrayList();
 		
 		
 	}
